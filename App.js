c9dfb2a530ba9db7f956a9d85cdc4b36a5ac9be3
Hunter <hvanlear@gmail.com>
2021-03-30 23:07:06 -0400

    refactor

---
+++ App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <Img />
+      <Img />
+      <NewImg />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ Img.js
@@ -0,0 +1,5 @@
+const Img = () => {
+  return (
+    <img src="https://images.unsplash.com/photo-1617119552123-60b37a5947fc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=940&q=80" />
+  );
+};
